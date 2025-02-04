5c1cc1693d484df47e682fc78c9514be9a4ad665
Ogm2211 <ogm2211@gmail.com>
2025-02-04 10:30:06 -0500

    initial commit

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="root-container flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        Too many attempt for short period!
+      </h1>
+      <p className="font-bebas-neue mt-5 text-light-100">
+        We've put a temporary pause on your excitement. Chill for a bit, and try
+        again shortly
+      </p>
+    </main>
+  );
+};
+
+export default page;
